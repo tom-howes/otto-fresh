d3cb91971cc616e1e34d563b8714275ad51776ef
paschalc24 <pcorrigan@umass.edu>
2026-02-01 21:49:38 -0500

    Replaced __init__.py with config.py and added callback url env variable

---
+++ backend/app/config.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# GitHub Environment Variables
+GITHUB_APP_ID = os.getenv("GITHUB_APP_ID")
+GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+GITHUB_PRIVATE_KEY_PATH = os.getenv("GITHUB_PRIVATE_KEY_PATH")
+GITHUB_CALLBACK_URL = os.getenv("GITHUB_CALLBACK_URL")
+
+with open(GITHUB_PRIVATE_KEY_PATH, "r") as f:
+  GITHUB_PRIVATE_KEY = f.read()
+
+# Firebase Environment Varibales
+FIREBASE_CREDENTIALS_PATH = os.getenv("FIREBASE_CREDENTIALS_PATH")
+FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
