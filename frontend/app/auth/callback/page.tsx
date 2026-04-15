1cf9ed5810a661fd2f754b2af8b98f10db8fc0cd
paschalc24 <pcorrigan@umass.edu>
2026-04-15 04:49:16 -0400

    Updated default navigation to board view

--- frontend/app/auth/callback/page.tsx
+++ frontend/app/auth/callback/page.tsx
@@ -22,7 +22,7 @@ const CallbackInner = () => {
 
       await refetchUser();
       await refetchWorkspaces();
-      router.replace("/project/backlog");
+      router.replace("/project/board");
     };
 
     void finishAuth();
